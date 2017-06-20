629574c9434b85ca762a7f74df97b9c301c608ca
Ling Situ <Situ@Lings-MacBook-Pro.local>
2017-06-20 13:05:50 -0700

    Using middleware to render HTML and an API route.

--- MERN-Sample/server.js
+++ MERN-Sample/server.js
@@ -1,11 +1,24 @@
 // const express = require("express");
 import express from "express";
+import apiRouter from './api';
+// import fs from "fs";
 
 const server = express();
 
 server.get('/',(req, res) => {
   res.send('Hello');
 });
+
+// Using middleware to render a html file
+server.use('/api', apiRouter);
+server.use(express.static("public"))
+
+// Rendering a html file
+// server.get('/about.html',(req, res) => {
+//   fs.readFile("./about.html", (err,data)=>{
+//     res.send(data.toString());
+//   });
+// });
 
 server.listen(3000, () => {
   console.info('Express listening on port 3000');
