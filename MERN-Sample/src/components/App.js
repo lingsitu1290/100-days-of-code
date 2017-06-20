33b935b671855403f47107ad140948357d6bbab7
Ling Situ <Situ@Lings-MacBook-Pro.local>
2017-06-20 15:25:27 -0700

    Changed to use class components to support state.

---
+++ MERN-Sample/src/components/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Header from "./Header"
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <Header message="Naming Contests"/>
+//       <div>
+//         ...
+//       </div>
+//     </div>
+//   );
+// };
+
+// Change to react class to introduce state or life cycle of the component
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { test: 42 };
+  // }
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+  render(){
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          // {this.state.test}
+          ...
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
