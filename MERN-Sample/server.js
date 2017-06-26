24d74deb120ee139e61dc17fca11b06c9167b41b
Ling Situ <lingsitu1290@gmail.com>
2017-06-26 13:00:31 -0700

    Added styling using sass. Installed node sass middelware. Linked css in server.js. Added class names in ContestPreview.js

--- MERN-Sample/server.js
+++ MERN-Sample/server.js
@@ -1,13 +1,15 @@
 // const express = require("express");
 import express from "express";
 import apiRouter from "./api";
-// import fs from "fs";
+import sassMiddleware from "node-sass-middleware";
+import path from "path";
 
 const server = express();
 
-// server.get("/",(req, res) => {
-//   res.send("Hello");
-// });
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public')
+}));
 
 // Set up EJS (Embedded JavaScript) will look for ejs files under a views folder
 server.set("view engine", "ejs");
@@ -23,13 +25,6 @@ server.get("/", (req, res) => {
 server.use("/api", apiRouter);
 server.use(express.static("public"))
 
-// Rendering a html file
-// server.get('/about.html',(req, res) => {
-//   fs.readFile("./about.html", (err,data)=>{
-//     res.send(data.toString());
-//   });
-// });
-
-server.listen(3000, () => {
-  console.info("Express listening on port 3000");
+server.listen(8080, () => {
+  console.info("Express listening on port 8080");
 });
